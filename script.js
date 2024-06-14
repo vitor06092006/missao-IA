9457e077e0d7f1bf7864ae24141ffecbe593af36
vitor06092006 <108538840+vitor06092006@users.noreply.github.com>
2024-06-14 11:30:21 -0300

    vv

---
+++ script.js
@@ -0,0 +1,25 @@
+const caixaPrincipal = document.querySeletor(".caixa-principal");
+const caixaPrincipal = document.querySeletor(".caixa-pergunta");
+const caixaPrincipal = document.querySeletor(".caixa-alternativas");
+const caixaPrincipal = document.querySeletor(".caixa-resultado");
+const caixaPrincipal = document.querySeletor(".texto-resultado");
+
+const = perguntas [
+    {
+    enunciado: "Pergunta 1",
+    alternativa: [
+        "alternativa 1",
+        "alternativa 2"
+    ]
+    },
+    {
+        enunciado: "Pergunta 2",
+        alternativa: [
+            "alternativa 1",
+            "alternativa 2"
+        
+        ]
+        }
+];
+
+
