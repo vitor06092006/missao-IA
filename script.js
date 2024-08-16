e39b42a4fe764a1c6979739b98f958284a5dc081
vitor06092006 <108538840+vitor06092006@users.noreply.github.com>
2024-08-16 11:57:00 -0300

    ok

--- script.js
+++ script.js
@@ -1,25 +1,57 @@
 const caixaPrincipal = document.querySeletor(".caixa-principal");
-const caixaPrincipal = document.querySeletor(".caixa-pergunta");
-const caixaPrincipal = document.querySeletor(".caixa-alternativas");
-const caixaPrincipal = document.querySeletor(".caixa-resultado");
-const caixaPrincipal = document.querySeletor(".texto-resultado");
+const caixaPerguntas = document.querySeletor(".caixa-pergunta");
+const caixaAlternativas = document.querySeletor(".caixa-alternativas");
+const caixaResultado = document.querySeletor(".caixa-resultado");
+const textoResultado = document.querySeletor(".texto-resultado");
 
-const = perguntas [
+// Array de objetos contendo as perguntas e alternativas
+const perguntas = [
     {
-    enunciado: "Pergunta 1",
-    alternativa: [
-        "alternativa 1",
-        "alternativa 2"
-    ]
+    enunciado: "Qual a raça de cachorro que possui orelhas longas e corpo comprido?",
+    alternativas: [
+        "Chihauhua",
+        "Dachshund"
+    ],
+    correta: 0 // A primeira alternativa é a correta
     },
     {
-        enunciado: "Pergunta 2",
-        alternativa: [
-            "alternativa 1",
-            "alternativa 2"
-        
-        ]
-        }
+    enunciado: "Qual a raça de cachorro que é de porte pequeno e são conhecidos por sua agrecividade?",
+    alternativas: [
+        "Pinscher",
+        "Pug"
+    ],
+    correta: 1 // A primeira alternativa é a correta
+    },
+    {
+    enunciado: "Qual a raça de cachorro que possui uma certa dificuldade em sua respiração, devido seu focinho achatado?",
+    alternativas: [
+        "Pug",
+        "Shih-tzu"
+    ],
+    correta: 1 // A primeira alternativa é a correta
+    },
+    {
+    enunciado: "Qual a raça de cachorro que são guias de pessoas com deficiencia visual?",
+    alternativas: [
+        "Rottwelier",
+        "Golden"
+    ],
+    correta: 0 // A primeira alternativa é a correta
+    },
+    {
+    enunciado: "Qual a raça de cachorro que são considerados pelos antepassados ótimos caçadores?",
+    alternativas: [
+        "Perdigueiro Português",
+        "Terrier Brasileiro"
+    ],
+    correta: 1 // A primeira alternativa é a correta
+    },
 ];
+
+let atual = 0;
+let perguntaAtual;
+let pontuacao = 0; // Inicie a pontuação em 0
+
+function montraPergunta() 
 
 
